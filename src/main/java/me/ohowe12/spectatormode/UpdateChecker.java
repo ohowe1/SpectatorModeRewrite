391609e23faa03ce11bf2c7d6cf7342c12794c9f
Oliver <ojhowe95@gmail.com>
2020-08-09 17:31:46 -0600

    Updated .gitignore

--- src/main/java/me/ohowe12/spectatormode/UpdateChecker.java
+++ src/main/java/me/ohowe12/spectatormode/UpdateChecker.java
@@ -20,22 +20,15 @@ import java.util.Scanner;
 
 public class UpdateChecker {
 
-    private final Plugin plugin;
-    private final int resourceId;
 
-    public UpdateChecker(Plugin plugin, int resourceId) {
-        this.plugin = plugin;
-        this.resourceId = resourceId;
-    }
-
-    public void getVersion(final @NotNull Consumer<String> consumer) {
-        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
-            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+    public static void getVersion(final @NotNull Consumer<String> consumer, Plugin plugin) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            try (InputStream inputStream = new URL("https://api.spiget.org/v2/resources/77267/versions/latest").openStream(); Scanner scanner = new Scanner(inputStream)) {
                 if (scanner.hasNext()) {
                     consumer.accept(scanner.next());
                 }
-            } catch (IOException exception) {
-                this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
     }
